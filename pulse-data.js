a20379b2afe32259df1eca1fd0ea519d07290e84
nitroideofficial <nitroideofficial@gmail.com>
2026-05-15 22:29:49 +0530

    Add files via upload

---
+++ pulse-data.js
@@ -0,0 +1,27 @@
+// pulse-data.js - Your Central Ecosystem Database
+const pulseLogs = [
+  {
+    date: "May 15, 2026",
+    platform: "Product Hunt",
+    icon: "ph-rocket-launch",
+    color: "#ff6154",
+    title: "Discussion: What if an online IDE felt instant?",
+    link: "https://www.producthunt.com/p/general/what-if-an-online-ide-felt-instant"
+  },
+  {
+    date: "May 14, 2026",
+    platform: "LinkedIn",
+    icon: "ph-linkedin-logo",
+    color: "#0a66c2",
+    title: "Why are cloud IDEs so slow? I built a local alternative.",
+    link: "https://www.linkedin.com/feed/update/urn:li:activity:7460748122923618304/"
+  },
+  {
+    date: "May 12, 2026",
+    platform: "X",
+    icon: "ph-twitter-logo",
+    color: "#1da1f2",
+    title: "We just pushed a massive update to the execution engine...",
+    link: "https://x.com/trynitroide/status/2054956322869899699"
+  }
+];
